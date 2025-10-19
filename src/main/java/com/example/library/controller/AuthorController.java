26fba7e3020bc26a8d2190880be720a27e342b13
Nosalts <atlasonb@gmail.com>
2025-10-19 17:40:58 +0200

    uppdaterat authorcontroller med en get

--- src/main/java/com/example/library/controller/AuthorController.java
+++ src/main/java/com/example/library/controller/AuthorController.java
@@ -3,11 +3,16 @@ package com.example.library.controller;
 import com.example.library.entity.Author;
 import com.example.library.service.AuthorService;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+// Denna controller hanterar allt som har med author o göra
+// hämta, söka och skapa nya authors
+
 
 @RestController
 @RequestMapping("/authors")
+
+// kopplar ihop controllern
 public class AuthorController {
     private final AuthorService service;
 
@@ -15,15 +20,24 @@ public class AuthorController {
         this.service = service;
     }
 
+    // GET /authors
+    // hämtar alla författare från databasen
+
     @GetMapping
     public List<Author> all() {
         return service.findAll();
     }
 
+    // GET /authors/name/{lastName}
+    // hittar en eller flera authors baserat på efternamn
+
     @GetMapping("/name/{lastName}")
     public List<Author> byLastName(@PathVariable String lastName) {
         return service.findByLastName(lastName);
     }
+
+    // POST /authors
+    // Skapar en ny author i databasen
 
     @PostMapping
     public Author create(@RequestBody Author a) {
